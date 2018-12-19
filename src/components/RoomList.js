5e7e7e2b0d3eb1a438e15b940ba713aef6206482
tar <rtorres845@gmail.com>
2018-12-19 15:46:40 -0600

    Chat Create Rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,11 +4,13 @@ class RoomList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rooms: []
+            rooms: [],
+            newRoomName: ""
         };
 
         this.roomsRef = this.props.firebase.database().ref('rooms');
-
+        this.onChange = this.onChange.bind(this);
+        this.newRoom = this.newRoom.bind(this);
     }
 
     componentDidMount() {
@@ -19,6 +21,18 @@ class RoomList extends Component {
         });
     }
 
+    onChange(event){
+        event.preventDefault();
+        this.setState({text: event.target.value});
+        this.roomsRef.push({name: this.state.newRoomName});
+
+    }
+
+    newRoom(event){
+        event.preventDefault();
+        this.setState({newRoomName: ''});
+    }
+
 
     render() {
         return (
@@ -26,11 +40,18 @@ class RoomList extends Component {
                 <div>
                     {this.state.rooms.map((room) => <li key={room.key}>{room.name}</li>)}
                 </div>
+            <form className="submit" onChange={this.onChange}>
+                <button type="submit">
+                    Create Room
+                </button>
+                <input type = "text" placeholder="New Room" value={this.state.newRoomName}  onChange={ this.onChange }/>
+            </form>
             </div>
 
 
         );
     }
 }
+
 
 export default RoomList;
