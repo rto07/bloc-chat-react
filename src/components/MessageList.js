de55595a8ecfa63c5453d334009b18be6cbdef92
tar <rtorres845@gmail.com>
2019-01-08 08:42:46 -0600

    list messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RoomList from './components/RoomList';
 
 class MessageList extends Component {
 	constructor(props, context){ 
@@ -11,50 +12,40 @@ class MessageList extends Component {
         this.messagesRef = this.props.firebase.database().ref('messages');	
         this.updateMessage = this.updateMessage.bind(this)
 		this.submitMessage = this.submitMessage.bind(this)
-    }
+    };
 
 	componentDidMount() {
 		console.log("componentDidMount");
 		this.messageListRef.on('child_added', snapshot => {
         	const newMessage = snapshot.val();
-            message.key = snapshot.key;
+            message.key = snapshot.key();
             this.setState({ messages:this.state.messages.concat(newMessage) });
-        })
+        
+        });
+	};
 
     updateMessage(event){
-    	console.log("updateMessage: " +event.target.value);
     	event.preventDefault();
-    	this.setState({
-    		message: event.target.value;
-    	})
-    }
+    	this.setState({message: event.target.value})
+    	console.log("updateMessage: " +event.target.value);
+
+    };
 
     submitMessage(event){
     	event.preventDefault();
     	console.log('submitMessage: '+this.state.message);
 		this.messagesref.push(this.state.nextMessage)
     	this.setState('message')}
-		// var list = Object.assign([], this.state.messages)
-		// list.push(nextMessage)
-		// this.setState({
-		// 	messages: list
-		// })
-	}
+	};
 
     render(){
-
-    	const currentMessage = this.state.messages.map((message, i) => {
-    			return(
-    				<li key={message.id}>{message.text}</li>
-    			)
-    	})
 
        return (
             <div className="messagelist">
                 <div>
                     {this.state.messages.map((message) => <ul key={message.key}>{message.name}</ul>)}
                 </div>
-            <form className="createMessage" onSubmit={this.newMessage}>
+            <form className="createMessage" onSubmit={this.submitMessage}>
                 <button type="submit">
                     New Message
                 </button>
